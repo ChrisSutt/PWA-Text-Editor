85288e7f28d0631c73608a1950b3b2d1856192a7
Chris <130914528+ChrisSutt@users.noreply.github.com>
2023-10-10 14:37:56 -0300

    updated all files to try to fix my install issue

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,73 +3,78 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-
 module.exports = () => {
   return {
     mode: 'development',
+    // Entry point for files
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js',
-      database: './src/js/database.js',
-      editor: './src/js/editor.js',
-      header: './src/js/header.js',
+      install: './src/js/install.js'
     },
+    // Output for our bundles
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
-    plugins: [
-      // Webpack plugin that generates our html file and injects our bundles
-      new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'JATE'
-      }),
-      // Injects our custom servie worker
-      new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'src-sw.js',
-      }),
-      // Creates a manifest.json file.
-      new WebpackPwaManifest({
-        fingerprints: false,
-        inject: true,
-        name: 'Just Another Text Editor',
-        short_name: 'JATE',
-        description: 'Just another text editor',
-        background_color: '#225ca3',
-        theme_color: '#225ca3',
-        start_url: '/',
-        publicPath: '/',
-        icons: [
-          {
-            src: path.resolve('src/images/logo.png'),
-            sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons'),
-          },
-        ],
-      }),
 
+    // Add and configure workbox plugins for a service worker and manifest file.
+    plugins: [
+     // Webpack plugin that generates our html file and injects our bundles. 
+     new HtmlWebpackPlugin({
+      // Creates a copy of the index.html file in the dist folder and inserts in script tag to the newly created bundle.js file
+      template: './index.html', 
+      // Optional parameters
+      title: 'Text Editor'
+    }),
+   
+    // Injects our custom service worker
+    new InjectManifest({
+      swSrc: './src-sw.js',
+      swDest: 'src-sw.js',
+    }),
+
+    // Creates a manifest.json file.
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: true,
+      name: 'Just another text editor',
+      short_name: 'JATE',
+      description: 'Just another text editor!',
+      background_color: '#225ca3',
+      theme_color: '#225ca3',
+      start_url: '/',
+      publicPath: '/',
+      icons: [
+        {
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },
+      ],
+    }),
     ],
 
     module: {
-      // CSS Loaders
+      // Add CSS loaders to webpack
       rules: [
         {
+          // This looks for a .css file and adds this into the bundle.js file
           test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
+          // Additional modules for converting the css into js
+          use: ["style-loader", "css-loader"]
         },
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
-          // We use babel-loader in order to use ES6.
+          // Add babel-loader to webpack in order to use ES6
           use: {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
-            },
-          },
-        },
+              presets: ["@babel/preset-env"],
+              plugins: ["@babel/plugin-proposal-object-rest-spread", "@babel/transform-runtime"]
+            }
+          }
+        }
       ],
     },
   };
