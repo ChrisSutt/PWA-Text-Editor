207b7c08ae911484a7e46f1b3a1486a89ddbf09f
Chris <130914528+ChrisSutt@users.noreply.github.com>
2023-10-10 13:24:32 -0300

    Had store.put had to change it to store.getAll in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,57 +1,52 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
-  export const putDb = async (content)  => {
-    console.log('PUT to the database');
-  
 
-    const contactDb = await openDB('jate', 1);
-  
+export const putDb = async (content) => {
 
-    const tx = contactDb.transaction('jate', 'readwrite');
-  
+  const db = await openDB("jate", 1);
 
-    const store = tx.objectStore('jate');
-  
 
-    const request = store.put({ id: 1, value: content });
-  
+  const tx = db.transaction("jate", "readwrite");
 
-    const result = await request;
-    console.log('🚀 - data saved to the database', result);
-  };
-  
-  // TODO: Add logic for a method that gets all the content from the database
-  export const getDb = async () => {
-    console.log('GET from the database');
-  
 
-    const contactDb = await openDB('jate', 1);
-  
+  const store = tx.objectStore("jate");
 
-    const tx = contactDb.transaction('jate', 'readonly');
 
-    const store = tx.objectStore('jate');
-  
+  const request = store.put({ id: 1, value: content });
 
-    const request = store.put();
-  
+  const result = await request;
+  console.log("data saved to the database", result);
+};
 
-    const result = await request;
-    console.log('result.value', result);
-    return result?.value;
-  };
-  
-  
-  initdb();
+
+export const getDb = async () => {
+
+  const db = await openDB("jate", 1);
+
+
+  const tx = db.transaction("jate", "readonly");
+
+
+  const store = tx.objectStore("jate");
+
+ 
+  const request = store.getAll();
+
+  const result = await request;
+  console.log("data read from database", result);
+  return result.value;
+};
+
+initdb();
