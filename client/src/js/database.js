606a271c000ab14be4551dd051e9343f660cf20e
Chris <130914528+ChrisSutt@users.noreply.github.com>
2023-10-10 11:39:34 -0300

    added comments to database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,44 +1,55 @@
 import { openDB } from 'idb';
 
+
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
+
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
         return;
       }
+
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
   });
 
-
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   try {
+    // Open the 'jate' database
     const db = await openDB('jate', 1);
 
+    // Begin a read-write transaction
     const tx = db.transaction('jate', 'readwrite');
 
+    // Access the 'jate' object store within the transaction
     const store = tx.objectStore('jate');
 
+    // Add the provided content to the object store
     const result = await store.add(content);
-    console.log('Content added to thje database', result);
+    console.log('Content added to the database', result);
     return result;
   } catch (error) {
     console.error('Error adding content to the database', error);
     throw error;
   }
 };
+
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   try {
+    // Open the 'jate' database
     const db = await openDB('jate', 1);
 
+    // Begin a read-only transaction
     const tx = db.transaction('jate', 'readonly');
     
+    // Access the 'jate' object store within the transaction
     const store = tx.objectStore('jate');
 
+    // Retrieve all content from the object store
     const result = await store.getAll();
     console.log('Received content from the database', result);
     return result;
@@ -48,5 +59,5 @@ export const getDb = async () => {
   }
 };
 
-
+// Initialize the database when the module is imported
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,40 @@
-const butInstall = document.getElementById('buttonInstall');
+// Get the reference to the install button
+const installButton = document.getElementById("buttonInstall");
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    // Prevent the default behavior of the install prompt
+    event.preventDefault();
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+    // Store the event for later use
+    window.deferredPrompt = event;
+
+    // Show the install button by removing the 'hidden' class
+    installButton.classList.toggle('hidden', false);
+});
+
+
+installButton.addEventListener('click', async () => {
+    // Retrieve the stored prompt event
+    const promptEvent = window.deferredPrompt;
+    
+    // Check if the prompt event is available
+    if (!promptEvent) {
+        return;
+    }
+
+    // Display the installation prompt to the user
+    promptEvent.prompt();
+
+    // Reset the deferred prompt variable as it can only be used once
+    window.deferredPrompt = null;
+
+    // Hide the install button after the prompt is shown
+    installButton.classList.toggle('hidden', true);
+});
+
+
+window.addEventListener('appinstalled', (event) => {
+    // Clear the stored prompt event after the app is successfully installed
+    window.deferredPrompt = null;
+});
