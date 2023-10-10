34b93e8a52a3c5bf5d81051eca3b0e3ec99d7056
Chris <130914528+ChrisSutt@users.noreply.github.com>
2023-10-10 13:41:43 -0300

    changed alot trying to get this to work

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -14,24 +12,24 @@ module.exports = () => {
       install: './src/js/install.js',
       database: './src/js/database.js',
       editor: './src/js/editor.js',
-      header: './src/js/header.js'
+      header: './src/js/header.js',
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-     
+
       new HtmlWebpackPlugin({
         template: './index.html',
         title: 'JATE'
       }),
-    
+
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
-     
+
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
@@ -54,6 +52,7 @@ module.exports = () => {
     ],
 
     module: {
+      // CSS Loaders
       rules: [
         {
           test: /\.css$/i,
@@ -61,16 +60,17 @@ module.exports = () => {
         },
         {
           test: /\.m?js$/,
-          exclude: /(node_modules|bower_components)/,
+          exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
           use: {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env'],
               plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
           },
         },
-      },
-    ],
-  },
- };
+      ],
+    },
+  };
 };
