0e655942eabb8bf4a60da8d073610ac7d2d6720f
Chris <130914528+ChrisSutt@users.noreply.github.com>
2023-10-10 11:21:44 -0300

    finished webpack.config, starting working on database.js adding logics to add content to database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,8 +13,22 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try {
+    const db = await dbPromise;
 
+    const tx = tb.transaction('jate', 'readwrite');
+
+    const store = tx.objectStore('jate');
+
+    const result = await store.add(content);
+    console.log('Content added to thje database', result);
+    return result;
+  } catch (error) {
+    console.error('Error adding content to the database', error);
+    throw error;
+  }
+};
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
 
